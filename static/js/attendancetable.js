874816738bf1367edd9cbb2a3f158e09d9284476
akshay toshniwal <akshaytoshniwal05@gmail.com>
2021-05-27 21:53:41 +0530

    added face module , timetable ui  updated

---
+++ static/js/attendancetable.js
@@ -0,0 +1,12 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  $('#dataTable').DataTable({
+    "bPaginate": true,
+    "bFilter": true,
+    "bSort":true,
+    "autoWidth": true,
+    "autoHeight":true
+  });
+
+  
+});
